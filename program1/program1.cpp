ac51bf1f66838131754bea8e957b94eb8d43f868
Dominik <99813661+TheFoxMaster17@users.noreply.github.com>
2022-05-16 11:02:52 +0200

    Add files via upload

---
+++ program1/program1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int money;
+    int nominaly[9];
+    nominaly[0] = 500;
+    nominaly[1] = 200;
+    nominaly[2] = 100;
+    nominaly[3] = 50;
+    nominaly[4] = 20;
+    nominaly[5] = 10;
+    nominaly[6] = 5;
+    nominaly[7] = 2;
+    nominaly[8] = 1;
+    int i = 0;
+    int f = 0;
+
+    cout << "Wpisz ile mam wyplacic: ";
+    cin >> money;
+    while (money >= 0)
+    {
+        if (money >= nominaly[i])
+        {
+            money = money - nominaly[i];
+            f++;
+        }
+        else
+        {
+            if (f > 0)
+            {
+                cout << nominaly[i] << " x " << f << endl;
+            }
+            i++;
+            f = 0;
+        }
+    }
+    return 0;
+}
